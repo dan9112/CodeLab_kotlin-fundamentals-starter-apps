35e6380af56afcc57a3c07d0a35d0d5b60ebd81d
dan9112_work_KamAZ <bessonovdanil9112@mail.ru>
2021-06-01 11:06:49 +0300

    структурные изменения
    Изменён способ перемещения между фрагментами на использование
    навигации

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -22,15 +22,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
 
 
 class GameWonFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_game_won, container, false)
+            inflater, R.layout.fragment_game_won, container, false
+        )
+        binding.nextMatchButton.setOnClickListener { view: View ->
+            view.findNavController()
+                .navigate(R.id.action_gameWonFragment_to_gameFragment)
+        }
+
+        setHasOptionsMenu(true)
+
         return binding.root
     }
 }
--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,13 +19,40 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
+    lateinit var navView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+        navView = binding.navView
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        NavigationUI.setupWithNavController(navView, navController)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return if (drawerLayout.isOpen) {
+            drawerLayout.close()
+            navController.navigateUp()
+        } else
+            NavigationUI.navigateUp(navController, drawerLayout)
     }
 
     // TODO (01) Create the new TitleFragment
